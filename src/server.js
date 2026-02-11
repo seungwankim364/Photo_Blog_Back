1a23a7ea2f099be9e42973e330278502e216db45
seungwan364 <seungwankim364@gmail.com>
2026-02-11 16:56:23 +0900

    Connect with frontend

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 import express from "express";
 import path from "path";
 import uploadRouter from "./routes/upload.js";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
 app.use("/uploads", express.static(path.resolve("uploads")));
 app.use("/upload", uploadRouter);
+app.use("/uploads", express.static("uploads"));
+app.use(cors());
 
 app.get("/health", (_, res) => {
   res.status(200).send("OK");
