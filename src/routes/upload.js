093142bdc017417a65b6e5d59037159bd21f826c
seungwankim364 <seungwankim364@github.com>
2026-03-06 16:34:00 +0900

    Remove: ACL public view

--- src/routes/upload.js
+++ src/routes/upload.js
@@ -22,7 +22,6 @@ const upload = multer({
   storage: multerS3({
     s3,
     bucket: S3_BUCKET,
-    acl: 'public-read',
     contentType: multerS3.AUTO_CONTENT_TYPE,
     key(req, file, cb) {
       const ext = path.extname(file.originalname).toLowerCase();
